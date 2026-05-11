7ec49a0ae0e24636ab7cd26c021c7c79cdccdd94
Danda Keerthi <dandakeerthi2@gmail.com>
2026-05-11 11:26:27 +0530

    Update 901_OnlineStockSpan.java

--- Stack/901_OnlineStockSpan.java
+++ Stack/901_OnlineStockSpan.java
@@ -2,7 +2,8 @@
 //SC:O(n) Because stack may store up to all previous prices in worst case.
 
 class StockSpanner {
-     class pair{
+     class pair{       //Because for every previous day, we need to store 2 things: price and span
+                       //So pair class groups both values into one object.Without pair class, stack can store only one value type.
         int price;
         int span;
 
