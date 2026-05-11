c56c2e7bc68bf5dc1c9b88c1b18b8e1c1c6892d5
Danda Keerthi <dandakeerthi2@gmail.com>
2026-05-11 13:27:46 +0530

    Create NextSmallerElement.java

---
+++ Stack/NextSmallerElement.java
@@ -0,0 +1,31 @@
+//TC:O(n)
+//SC:O(n)
+
+class Solution {
+    static ArrayList<Integer> nextSmallerEle(int[] arr) {
+        
+        int n=arr.length;
+        Stack<Integer> s= new Stack<>();
+        
+        int nextSmaller[]=new int[n];
+        
+        for(int i=n-1;i>=0;i--){
+            while(!s.isEmpty() && s.peek()>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nextSmaller[i]=-1;
+            }else{
+                nextSmaller[i]=s.peek();
+            }
+            s.push(arr[i]);
+        }
+        
+            
+            ArrayList<Integer> ans= new ArrayList<>();
+            for(int i=0;i<nextSmaller.length;i++){
+                ans.add(nextSmaller[i]);
+            }
+            return ans;
+    }
+}
