db8670e224476761d965dd5fac826836afd749a3
Danda Keerthi <dandakeerthi2@gmail.com>
2026-05-13 13:25:53 +0530

    Update 232_ImplementQueueUsingStack.java

--- Stack/232_ImplementQueueUsingStack.java
+++ Stack/232_ImplementQueueUsingStack.java
@@ -34,5 +34,53 @@ class MyQueue {
         return s1.isEmpty();
     }
 }
+=====================================================================================================================================================
+//Approach 2:
+//pop O(n) and peek O(n)
+//push O(1)
+    class MyQueue {
+
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    public MyQueue() {
+        s1=new Stack<>();
+        s2=new Stack<>();
+    }
+    
+    public void push(int x) {
+        s1.push(x);
+    }
+    
+    public int pop() {
+       while(!s1.isEmpty()){
+        s2.push(s1.pop());
+       }
+
+       int result=s2.pop();
+
+       while(!s2.isEmpty()){
+        s1.push(s2.pop());
+       }
+       return result;
+    }
+    
+    public int peek() {
+        while(!s1.isEmpty()){
+        s2.push(s1.pop());
+       }
+
+       int result=s2.peek();
+
+       while(!s2.isEmpty()){
+        s1.push(s2.pop());
+       }
+       return result;
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
 
 
