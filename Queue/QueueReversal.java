c1d21706b5613563933e993a3c292c281130b1e1
Danda Keerthi <dandakeerthi2@gmail.com>
2026-05-15 11:30:42 +0530

    Create QueueReversal.java

---
+++ Queue/QueueReversal.java
@@ -0,0 +1,17 @@
+//TC:O(n)
+//SC:O(n) due to stack
+
+class Solution {
+    public void reverseQueue(Queue<Integer> q) {
+        // code here
+        Stack<Integer> s= new Stack<>();
+        
+        while(!q.isEmpty()){
+            s.push(q.remove());
+        }
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+    }
+    
+}
