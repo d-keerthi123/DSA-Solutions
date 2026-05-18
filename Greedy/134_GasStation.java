42ba14140e75babb445e58e800a9223c56adcc80
Danda Keerthi <dandakeerthi2@gmail.com>
2026-05-18 12:02:15 +0530

    Update 134_GasStation.java

--- Greedy/134_GasStation.java
+++ Greedy/134_GasStation.java
@@ -1,3 +1,4 @@
+//Approach 1:Brute Force
 //Tc:O(n^2)
 //SC:O(1)
 
@@ -30,5 +31,37 @@ class Solution {
             }
         }
         return -1;
+    }
+}
+
+======================================================================================================================================================
+//Approach 2: Greedy
+//TC:O(n)
+//SC:O(1)
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n=gas.length;
+        int totalGas=0;
+        int totalCost=0;
+        
+
+        for(int i=0;i<n;i++){
+            totalGas+=gas[i];
+            totalCost+=cost[i];
+        }
+        if(totalGas < totalCost){
+            return -1;
+        }
+        int total=0;
+        int result=0;
+        for(int i=0;i<n;i++){
+            total=total+gas[i]-cost[i];
+            if(total<0){ //total can't be -ve 
+                total=0;
+                result=i+1;//update result
+            }
+        }
+        return result;
     }
 }
