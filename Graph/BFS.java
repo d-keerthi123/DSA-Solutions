7b731132c329082222b2dc093def1bcc503961d8
Danda Keerthi <dandakeerthi2@gmail.com>
2026-05-28 09:56:53 +0530

    Create BFS.java

---
+++ Graph/BFS.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        boolean visited[]= new boolean[adj.size()];
+        ArrayList<Integer> ans= new ArrayList<>();
+        Queue<Integer> q= new LinkedList<>();
+        
+        q.add(0);//src 0
+        
+        while(!q.isEmpty()){
+            
+            int curr=q.remove();
+            
+            if(!visited[curr]){
+                
+                visited[curr]=true;
+                
+                ans.add(curr);
+                
+                //get neighbours of curr node 
+                for(int i=0;i<adj.get(curr).size();i++){
+                   int neighbour =adj.get(curr).get(i); 
+                    q.add(neighbour);
+                }
+            }
+        }
+        return ans;
+    }
+}
