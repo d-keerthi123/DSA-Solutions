0bf2f222733d79b4044d22c24c279e963a8273be
Danda Keerthi <dandakeerthi2@gmail.com>
2026-06-17 18:41:17 +0530

    Update 70_Climbing Stairs.java

--- DP/70_Climbing Stairs.java
+++ DP/70_Climbing Stairs.java
@@ -17,8 +17,8 @@ class Solution {
 //TC:O(n)
 class Solution {
     public int climbStairs(int n){
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
+        int[] dp=new int[n+1]; //0 0 0....
+        Arrays.fill(dp,-1);//-1 -1 -1....
         return climbStairsM(n,dp);
     }
     public int climbStairsM(int n,int[]dp) {
@@ -29,10 +29,26 @@ class Solution {
         if(n<0){
             return 0;
         }
-        if(dp[n] != -1){
+        if(dp[n] != -1){ //already calculated
             return dp[n];
         }
         dp[n]=climbStairsM(n-1,dp) +climbStairsM(n-2,dp);
+        return dp[n];
+    }
+}
+
+
+//Approach 3:Tabulation
+//TC:O(n)
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
 }
