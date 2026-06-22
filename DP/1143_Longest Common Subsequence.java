daa25c413a6a0743216faee368accb8eefb10687
Danda Keerthi <dandakeerthi2@gmail.com>
2026-06-22 12:35:38 +0530

    Update 1143_Longest Common Subsequence.java

--- DP/1143_Longest Common Subsequence.java
+++ DP/1143_Longest Common Subsequence.java
@@ -64,3 +64,36 @@ class Solution {
         return dp[n][m]=Math.max(ans1,ans2);
     }
 }
+=========================================================================================================================================================
+//Approach 3:Tabulation
+//TC:O(nxm)
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2){
+        int n=text1.length();
+        int m=text2.length();
+
+         // Base case initialization
+        int dp[][]= new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=0;
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1] +1;
+                }
+                else{
+                    int ans1=dp[i-1][j];
+                    int ans2=dp[i][j-1];
+                    dp[i][j]=Math.max(ans1,ans2);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
