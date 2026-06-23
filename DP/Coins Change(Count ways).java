ce58bf925a7624d458e202ba30dd1e7f258dbaa3
Danda Keerthi <dandakeerthi2@gmail.com>
2026-06-23 16:45:50 +0530

    Create Coins Change(Count ways).java

---
+++ DP/Coins Change(Count ways).java
@@ -0,0 +1,32 @@
+//Approach :Tabulation
+//TC:O(nxsum)
+class Solution {
+    public int count(int coins[], int sum) {
+        // code here.
+        int n=coins.length;
+        int dp[][]=new int[n+1][sum+1];
+        
+        //base case initialization
+        for(int i=0;i<=n;i++){ //col
+            dp[i][0]=1;
+        }
+        for(int j=1;j<=sum;j++){ //row
+            dp[0][j]=0;
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                //valid 
+                if(coins[i-1]<=j){
+                    dp[i][j]=dp[i][j-coins[i-1]] + dp[i-1][j];
+                }
+                else{
+                    //in valid---> exclude 
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum];
+    }
+}
