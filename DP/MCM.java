ea38794e81e6bb5451cc51a55b2e417df7a2e6e1
Danda Keerthi <dandakeerthi2@gmail.com>
2026-06-29 11:33:05 +0530

    Update MCM.java

--- DP/MCM.java
+++ DP/MCM.java
@@ -70,3 +70,37 @@ class Solution {
         return dp[i][j]=ans; 
     }
 }
+
+=====================================================================================================================================================
+//Approach:Tabulation
+//TC:O(n^2)
+    
+class Solution {
+    static int matrixMultiplication(int arr[]) {
+        int n=arr.length;
+        int dp[][]=new int[n][n];
+        
+        //base case initialization
+        for(int i=0;i<n;i++){
+            dp[i][i]=0;
+        }
+        
+        for(int len=2;len<=n-1;len++){
+            for(int i=1;i<=n-len;i++){ //row
+                int j=i+len-1; //col
+                
+                dp[i][j]=Integer.MAX_VALUE;
+                for(int k=i;k<=j-1;k++){ //for cuts
+                    int cost1=dp[i][k];
+                    int cost2=dp[k+1][j];
+                    int cost3=arr[i-1] *arr[k]*arr[j];
+                    
+                    dp[i][j]=Math.min(dp[i][j], cost1+cost2+cost3);
+                    
+                }
+            }
+        }
+        
+        return dp[1][n-1];
+    }
+}
